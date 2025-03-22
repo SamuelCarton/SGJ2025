9267569815b3c672c717593c7e48de27676479cb
Raphael Bonneval <grugroo128@gmail.com>
2025-03-22 09:37:50 +0100

    Init Scriptable Objects

---
+++ Assets/Scripts/Plant_Data.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlantData", menuName = "SGJ2025/PlantData")]
+public class Plant_Data : ScriptableObject
+{
+    public List<Sprite> plantSprites;
+}
