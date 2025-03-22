9fe4a6ecc2a528d4b9e39e372507b0db0cff6802
Raphael Bonneval <grugroo128@gmail.com>
2025-03-22 12:09:19 +0100

    ADD UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    [SerializeField] private PotUI potUI;
+    [SerializeField] private ScannerUI scannerUI;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OpenPotUI(Pot pot)
+    {
+        
+    }
+}
