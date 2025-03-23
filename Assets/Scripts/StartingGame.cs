fa8a768873b44ea9d36d1cd8538d646a9cb5d2e3
Piot Erwan <pioterwan2@gmail.com>
2025-03-23 14:25:42 +0100

    Text modifier

--- Assets/Scripts/StartingGame.cs
+++ Assets/Scripts/StartingGame.cs
@@ -7,6 +7,9 @@ public class StartingGame : MonoBehaviour
     private IEnumerator Start()
     {
         yield return new WaitForSecondsRealtime(1.0f);
-        UIManager.Instance.OpenPopup("Mars fait l’objet de convoitises depuis plusieurs siècles, de nos jours l’Homme est sur le point de réaliser ce grand rêve. Vous avez été envoyé pour contribuer à cet énorme projet en tant que cultivateur sur le sol martien avant l’arrivée de plusieurs colons.");
+        UIManager.Instance.OpenPopup("" +
+            "Mars fait l’objet de convoitises depuis plusieurs siècles, de nos jours l’Homme est sur le point de réaliser ce grand rêve. " +
+            "Vous avez été choisis en tant qu'agriculteur martien pour préparer l’arrivée de plusieurs colons."
+            );
     }
 }
